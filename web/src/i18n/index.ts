147f3243e288ec015ba14dc3a18bc698c2991aee
Killiann Hervagault <killiannhervagault@gmail.com>
2026-05-22 01:17:06 +0000

    fix

--- web/src/i18n/index.ts
+++ web/src/i18n/index.ts
@@ -1,3 +1,4 @@
+// web/src/i18n/index.ts
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./locales/en";
@@ -7,8 +8,8 @@ i18n
   .use(initReactI18next)
   .init({
     resources: { en: { translation: en }, fr: { translation: fr } },
-    fallbackLng: "en",
     lng: "en",
+    fallbackLng: "en",
     defaultNS: "translation",
     interpolation: { escapeValue: false },
   });
