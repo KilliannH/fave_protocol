74c8eb29967015bda01ed08a00f5212bb04f8a7a
Killiann Hervagault <killiannhervagault@gmail.com>
2026-05-22 01:31:55 +0000

    yes

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./i18n";
+localStorage.removeItem('i18nextLng');
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
